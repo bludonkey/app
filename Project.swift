20c25ad7235397114517b9e035b36a6485afd60e
vauxhall <zero.griffin@gmail.com>
2020-10-27 12:08:58 +0100

    Update project

--- Project.swift
+++ Project.swift
@@ -3,12 +3,24 @@ import MapKit
 
 struct Project: View {
     @Binding var project: Model?
+    @State private var list = false
+    
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
     
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $region)
-                .edgesIgnoringSafeArea(.horizontal)
+            VStack {
+                Map(coordinateRegion: $region)
+                    .edgesIgnoringSafeArea(.horizontal)
+                if list {
+                    ScrollView {
+                        HStack {
+                            Text("hello world")
+                            Spacer()
+                        }
+                    }.frame(height: 250)
+                }
+            }
             VStack {
                 Spacer()
                 HStack {
@@ -16,8 +28,10 @@ struct Project: View {
                     Control.Circle(image: "dial.max.fill") {
                         
                     }
-                    Control.Circle(image: "list.bullet") {
-                        
+                    Control.Circle(state: list ? .selected : .ready, image: "list.bullet") {
+                        withAnimation(.easeInOut(duration: 1)) {
+                            list.toggle()
+                        }
                     }
                     Control.Circle(image: "location.fill.viewfinder") {
                         
